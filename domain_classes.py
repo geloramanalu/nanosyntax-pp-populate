3f6a9a43553e574e4a48ab88ae67cbc7068f75dd
Gelora Manalu <gelora.dam2003@mail.ugm.ac.id>
2025-06-05 15:51:24 +0700

    decomposed and added list of extra pps

--- domain_classes.py
+++ domain_classes.py
@@ -6,12 +6,32 @@ class K:
 class AxPart:
     def apply(self, region):
         print(f"subpart({region})")
-        return region
+        
+    def validate(self):
+        if followed_by_particle(self):
+            return False
+        else:
+            return True
     
-class Loc:
+    def followed_by_particle(self):
+        # if followed by "the" or "a", return True
+        # otherwise return False
+        pass
+    
+class Proj:
     def apply(self, axpart_region):
         print(f"vector_space({axpart_region})")
         return axpart_region
+    def validate(self):
+        if followed_by_measP(self):
+            return False
+        else:
+            return True
+        
+    def followed_by_measP(self):
+        # if followed by measure phrase like "one meter" or other units, return True
+        # otherwise return False
+        pass
 
 class Deg:
     def apply(self, vector_space):
@@ -24,7 +44,7 @@ class Place:
         self.ground = ground
         self.k = K()
         self.axpart = AxPart()
-        self.loc = Loc()
+        self.loc = Proj()
         self.deg = Deg()
         
     def derive(self):
